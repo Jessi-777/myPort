7e5f9ab84b5b529201ed442eb6d2d04442a90bef
Jessi <jcsoftwareengineer369@gmail.com>
2025-07-22 19:35:30 -0700

    2nd commit

--- backend/models/Film.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const filmSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  director: { type: String },
-  duration: { type: Number }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Film', filmSchema);
--- backend/models/Music.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const musicSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  artist: { type: String },
-  genre: { type: String }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Music', musicSchema);
