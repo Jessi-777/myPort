0658eb079bb1037c401ea8327ffdcac093769821
Jessi <jcsoftwareengineer369@gmail.com>
2026-05-15 19:31:30 -0700

    updated cors with vercel link

--- backend/index.js
+++ backend/index.js
@@ -57,11 +57,11 @@ const allowedOrigins = [
   "http://127.0.0.1:5176",
   'http://localhost:5173',
   'https://my-port-quejf6t9u-jessi777s-projects.vercel.app',
-  'https://jcsoftwareengineer.com',      // ADD THIS
-  'https://www.jcsoftwareengineer.com', 
+  // 'https://jcsoftwareengineer.com',      // ADD THIS
+  // 'https://www.jcsoftwareengineer.com', 
   'https://business-app-template1.vercel.app',
   // "https://my-port-phi-lake.vercel.app",
-  // "https://my-port-8bj98t7cw-jessi777s-projects.vercel.app",
+  "https://my-port-8bj98t7cw-jessi777s-projects.vercel.app",
   clientUrl
 ].filter(Boolean);
 
