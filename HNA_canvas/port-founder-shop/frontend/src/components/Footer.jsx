1bb1d94d1d6774c612d66a00abffd0f7ea42824d
Jessi <jcsoftwareengineer369@gmail.com>
2026-05-15 17:25:04 -0700

    Remove unrelated project HNA_canvas from repo

--- HNA_canvas/port-founder-shop/frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-[#576F4D] text-[#47648c] text-center py-4 mt-auto">
-      &copy; {new Date().getFullYear()} HNA. All rights reserved.
-    </footer>
-  );
-}
